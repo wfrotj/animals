47f60a8aa38a010d9126c55ca9bf863979555fd4
wfortj <trebrodrigo@gmail.com>
2023-04-02 10:48:31 +0800

    add java

---
+++ sounds/script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("chicken");
+
+function playMusic() {
+  const audio = new Audio("chick.mp3");
+  audio.play();
+}
+
+button.addEventListener("click", playMusic);
