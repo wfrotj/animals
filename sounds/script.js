f2845043cff44534c4d92a916584486921095076
wfortj <trebrodrigo@gmail.com>
2023-04-02 11:59:39 +0800

    hover

--- sounds/script.js
+++ sounds/script.js
@@ -1,9 +1,15 @@
-const button = document.getElementById("chicken");
+const button1 = document.getElementById("chicken");
+const button2 = document.getElementById("cow");
+const button3 = document.getElementById("bird");
 
 function playMusic() {
   const audio = new Audio("chick.mp3");
   audio.play();
 }
+function playMusic() {
+  const audio = new Audio("cow.mp3");
+  audio.play();
+}
 
-button.addEventListener("click", playMusic);
-button.addEventListener("mouseover");
+button1.addEventListener("click", playMusic);
+button2.addEventListener("click", playMusic);
